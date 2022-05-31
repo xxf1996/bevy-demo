73588c42cc1319a9bd6682a54c2c5f64a052da27
xiexuefeng <1158231926@qq.com>
2022-05-31 18:48:58 +0800

    feat: 测试ECS流程

--- Readme.md
+++ Readme.md
@@ -1,1 +1,21 @@
 - https://bevyengine.org/learn/book/getting-started/setup/
+
+## 注意事项
+
+- 在Mac端安装zld的时候（`brew install michaeleisel/zld/zld`），可能会出现如下报错：
+
+  ```bash
+  xcode-select: error: tool 'xcodebuild' requires Xcode, but active developer directory '/Library/Developer/CommandLineTools' is a command line tools instance
+  ```
+
+  可以考虑使用如下命令来解决[^1]：
+
+  ```bash
+  sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer/
+  ```
+
+  
+
+- 
+
+[^1]: [xcode-select: error: tool 'xcodebuild' requires Xcode - 简书](https://www.jianshu.com/p/07a281ff57d3)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,56 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32
+}
+
+#[derive(Component)]
+struct Name(String);
+
+pub struct DemoPlugin;
+struct DemoTimer(Timer);
+
+impl Plugin for DemoPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(DemoTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(setup)
+            .add_system(view_postions);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn().insert(Position {
+        x: 1.2,
+        y: 2.2
+    }).insert(Name("P1".to_string()));
+    commands.spawn().insert(Position {
+        x: -22.3,
+        y: 0.45
+    });
+    commands.spawn().insert(Position {
+        x: -1.3,
+        y: 34.45
+    }).insert(Name("P3".to_string()));
+}
+
+fn view_postions(time: Res<Time>, mut timer: ResMut<DemoTimer>, query: Query<&Position, With<Name>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        say_hello();
+        for pos in query.iter() {
+            println!("position is ({}, {})!", pos.x, pos.y);
+        }
+    }
+}
+
 fn say_hello() {
     println!("Hello, world!");
 }
 
 fn main() {
-    App::new().add_system(say_hello).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(DemoPlugin)
+        .run();
 }

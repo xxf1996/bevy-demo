3db0e55c86e3feb46099fe0bfe6632b95816edb0
xiexuefeng <1158231926@qq.com>
2022-06-09 18:53:27 +0800

    feat: wasm编译测试&替换gltf材质测试

--- Readme.md
+++ Readme.md
@@ -1,5 +1,31 @@
 - https://bevyengine.org/learn/book/getting-started/setup/
 
+## 常用命令
+
+- 编译为wasm并运行指定example：
+
+  ```bash
+  cargo run --example obj_custom --target wasm32-unknown-unknown
+  ```
+
+  更多基于bevy编译为wasm的细节可以参考：[Browser (WebAssembly) - Unofficial Bevy Cheat Book](https://bevy-cheatbook.github.io/platforms/wasm.html)
+
+- 一般运行指定example：
+
+  ```bash
+  cargo run --example obj_custom -F bevy/dynamic
+  ```
+
+  由于wasm32不支持dynamic特征，所以在cargo.toml中就没有直接开启，因此可以在命令通过`--features`开启，进行编译加速（有没有办法配置不同的target使用不同的features？）；
+
+- 
+
+
+
+
+
+
+
 ## 注意事项
 
 - 在Mac端安装zld的时候（`brew install michaeleisel/zld/zld`），可能会出现如下报错：
@@ -29,6 +55,12 @@
 
   
 
+- 开启bevy的dynamic feature后，编译wasm会出现报错：
+
+  ![image-20220609151452684](http://pic.xiexuefeng.cc/markdown/image-20220609151452684.png?imageslim)
+
+  原因是开启这个特征后bevy会使用bevy_dylib这个库；可以关闭这个特征，编译wasm就会成功，但是**编译速度很慢**。目前在网上找到一个说法是dynamic特征只适合于桌面端target[^3]，因此wasm32应该不包含在内？
+
 - 
 
 
@@ -45,3 +77,4 @@
 
 [^1]: [xcode-select: error: tool 'xcodebuild' requires Xcode - 简书](https://www.jianshu.com/p/07a281ff57d3)
 [^2]: https://github.com/bevyengine/bevy/issues/1391#issuecomment-773428852
+[^3]: https://bevy-cheatbook.github.io/setup/bevy-config.html?highlight=dynamic#dynamic-linking
--- example/gltf_load.rs
+++ example/gltf_load.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, gltf::{Gltf, GltfMesh}};
+use bevy::{prelude::*, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, gltf::{Gltf, GltfMesh, GltfNode}};
 use smooth_bevy_cameras::{
   controllers::orbit::{
     OrbitCameraBundle,
@@ -13,27 +13,82 @@ struct GltfAssetPack(Handle<Gltf>);
 struct GltfLoaded(bool); // 自定义状态，参考：https://bevy-cheatbook.github.io/programming/states.html
 
 /// 参考：https://bevy-cheatbook.github.io/3d/gltf.html
-fn custom_gltf(mut commands: Commands, gltf_pack: Res<GltfAssetPack>, gltf_assets: Res<Assets<Gltf>>, gltf_mesh: Res<Assets<GltfMesh>>, mut gltf_loaded: ResMut<State<GltfLoaded>>) {
+fn custom_gltf(
+  mut commands: Commands,
+  gltf_pack: Res<GltfAssetPack>,
+  gltf_assets: Res<Assets<Gltf>>,
+  gltf_mesh: Res<Assets<GltfMesh>>,
+  gltf_nodes: Res<Assets<GltfNode>>,
+  mut gltf_loaded: ResMut<State<GltfLoaded>>
+) {
   if let GltfLoaded(true) = gltf_loaded.current() {
     return;
   }
   if let Some(gltf) = gltf_assets.get(&gltf_pack.0)  {
-    for mesh_id in gltf.meshes.iter() { // 遍历场景的物体
-      let mesh = gltf_mesh.get(&*mesh_id).unwrap();
-      commands.spawn_bundle(PbrBundle { // 重新组合mesh和material
-        mesh: mesh.primitives[0].mesh.clone(),
-        material: mesh.primitives[0].material.clone().unwrap(),
-        ..default()
-      });
+    // for mesh_id in gltf.meshes.iter() { // 遍历场景的物体
+    //   let mesh = gltf_mesh.get(&*mesh_id).unwrap();
+    //   commands.spawn_bundle(PbrBundle { // 重新组合mesh和material
+    //     mesh: mesh.primitives[0].mesh.clone(),
+    //     material: mesh.primitives[0].material.clone().unwrap(),
+    //     ..default()
+    //   });
+    // }
+    let mut hanler = |node: &GltfNode, parent_transform: Option<Transform>| {
+      let mesh_id = node.mesh.as_ref().unwrap();
+      let mesh = gltf_mesh.get(mesh_id).unwrap();
+      println!("primitive num: {}", mesh.primitives.len());
+      for obj in mesh.primitives.iter() {
+        commands.spawn_bundle(PbrBundle {
+          mesh: obj.mesh.clone(),
+          material: obj.material.clone().unwrap(),
+          transform: if let None = parent_transform {
+            node.transform.clone()
+          } else {
+            parent_transform.unwrap().mul_transform(node.transform.clone())
+          },
+          ..default()
+        });
+      }
+    };
+    for node_id in gltf.nodes.iter() {
+      let node = gltf_nodes.get(node_id).unwrap();
+      if let None = node.mesh {
+        println!("transform: {:?}", node.transform);
+        // for child_node in node.children.iter() {
+        //   hanler(&child_node, Some(node.transform.clone()));
+        // }
+        continue;
+      }
+      hanler(&node, None);
+    }
+    gltf_loaded.set(GltfLoaded(true)).unwrap();
+  }
+}
+
+fn replace_material<T>(
+  // mut commands: Commands,
+  gltf_assets: Res<Assets<Gltf>>,
+  mut gltf_mesh: ResMut<Assets<GltfMesh>>,
+  mut gltf_loaded: ResMut<State<GltfLoaded>>
+) {
+  if let GltfLoaded(true) = gltf_loaded.current() {
+    return;
+  }
+  if gltf_assets.len() > 0 {
+    println!("mesh num: {}", gltf_mesh.len());
+    for (_id, mesh) in gltf_mesh.iter_mut() {
+      for obj in mesh.primitives.iter_mut() {
+        obj.material = obj.material.clone();
+      }
     }
     gltf_loaded.set(GltfLoaded(true)).unwrap();
   }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-  // commands.spawn_scene(asset_server.load("model/realistic interior/realistic interior.glb#Scene0")); // 需要指定模型名称（#后面的名称），不然加载为空
-  let gltf = asset_server.load("model/realistic interior/realistic interior.glb");
-  commands.insert_resource(GltfAssetPack(gltf));
+  commands.spawn_scene(asset_server.load("model/realistic interior/realistic interior.glb#Scene0")); // 需要指定模型名称（#后面的名称），不然加载为空
+  // let gltf = asset_server.load("model/realistic interior/realistic interior.glb");
+  // commands.insert_resource(GltfAssetPack(gltf));
   commands.spawn_bundle(OrbitCameraBundle::new(
     OrbitCameraController::default(),
     PerspectiveCameraBundle {
@@ -63,6 +118,7 @@ fn main() {
     .add_plugin(OrbitCameraPlugin::default())
     .add_state(GltfLoaded(false))
     .add_startup_system(setup)
-    .add_system(custom_gltf)
+    // .add_system(custom_gltf)
+    .add_system(replace_material)
     .run();
 }
